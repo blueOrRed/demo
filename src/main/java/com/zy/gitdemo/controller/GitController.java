2c200de8314a6c774f5707bf8ba023c8c04706e5
zydemo <1242638454@qq.com>
2020-04-16 09:45:06 +0800

    增加一个登录demo

--- src/main/java/com/zy/gitdemo/controller/GitController.java
+++ src/main/java/com/zy/gitdemo/controller/GitController.java
@@ -18,4 +18,24 @@ public class GitController {
     public void localPrint(){
         System.out.println("====我是一个无情的打印机器======");
     }
+
+
+    @GetMapping("login")
+    public String login(String username,String password){
+        //先判断大的
+        if((username ==null || "".equals(username.trim())) && (password==null || "".equals(password.trim()))){
+            return "用户名和密码不得为空!";
+        }
+
+        if(username ==null || "".equals(username)){
+            return "用户名不能为空!";
+    }
+        if(password==null || "".equals(password)){
+        return "密码不能为空!";
+    }
+        if(username.equals("zyHelloWorld") && password.equals("zyaile00")){
+            return "登录成功!";
+        }
+        return "登录失败！请重新输入用户名或密码!!";
+    }
 }
